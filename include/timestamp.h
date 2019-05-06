17c20ca585936b9db9c0e4dabfd09eed79b42740
Martin Schröder <mkschreder.uk@gmail.com>
2019-05-06 15:50:09 +0200

    rewrite timestamp code completely.
    Use a dedicated struct to discourage direct access to time value.
    
    All time operations on time should be done through the timestamp_*
    methods.

--- include/timestamp.h
+++ include/timestamp.h
@@ -26,11 +26,38 @@
 
 struct timeval;
 
-typedef unsigned long timestamp_t;
-typedef unsigned long usec_t;
+typedef uint32_t sec_t;
+typedef int32_t sec_diff_t;
+typedef uint32_t msec_t;
+typedef int32_t msec_diff_t;
+typedef uint32_t usec_t;
+typedef int32_t usec_diff_t;
 
-// time_after(a,b) returns true if the time a is after time b.
+typedef struct {
+	sec_t sec;
+	usec_t usec;
+} timestamp_t;
 
+typedef struct {
+	sec_diff_t sec;
+	usec_diff_t usec;
+} timestamp_diff_t;
+
+//typedef unsigned long timestamp_t;
+//typedef unsigned long usec_t;
+
+int timestamp_before(const timestamp_t a, const timestamp_t b);
+int timestamp_after(const timestamp_t a, const timestamp_t b);
+int timestamp_expired(const timestamp_t a);
+timestamp_t timestamp_add(const timestamp_t a, const timestamp_t b);
+timestamp_diff_t timestamp_sub(const timestamp_t a, const timestamp_t b);
+timestamp_t timestamp_add_ms(const timestamp_t a, const msec_t ms);
+timestamp_t timestamp_add_us(const timestamp_t a, const usec_t us);
+timestamp_t timestamp_from_now_us(usec_t us);
+timestamp_t timestamp_from_now_ms(msec_t ms);
+timestamp_t timestamp();
+
+#if 0
 #define time_before(a,b)		\
 	(typecheck(uint32_t, a) && \
 	 typecheck(uint32_t, b) && \
@@ -42,20 +69,9 @@ typedef unsigned long usec_t;
 	 typecheck(unsigned long, b) && \
 	 (((long)(a) - (long)(b)) >= 0))
 #define time_before_eq(a,b)	time_after_eq(b,a)
+#endif
 
-void delay_us(uint32_t us);
-uint32_t nanos();
-
-// should return exact clock speed (platform specific)
-uint32_t time_get_clock_speed();
-int time_cpu_clock_speed_exact();
-
-/*
- * Calculate whether a is in the range of [b, c].
- */
-#define time_in_range(a,b,c) \
-	(time_after_eq(a,b) && \
-	 time_before_eq(a,c))
+//uint32_t nanos();
 
 int timeval_subtract(const struct timeval *x, const struct timeval *y, struct timeval *result);
 //int time_diff(const struct timeval *x, const struct timeval *y, struct timeval *result);
@@ -64,6 +80,10 @@ void time_cpy(struct timeval *dst, const struct timeval *src);
 void time_init(void);
 int time_init_rtc(void);
 
-timestamp_t micros(void);
+usec_t micros(void);
 void time_gettime(struct timeval *ts);
 uint32_t time_get_hse_precision(void);
+
+void delay_us(uint32_t us);
+uint32_t time_get_clock_speed();
+int time_cpu_clock_speed_exact();
